e8728ac0e83a43e6144a5563c2bb4bbd1bdf2d3f
NamkyuHan <rksh333@naver.com>
2020-11-27 19:19:51 +0900

    2020.11.27 프로젝트분

--- project/main.py
+++ project/main.py
@@ -59,4 +59,127 @@ df = df[df['country'].isin(fc)].reset_index(drop=True)
 print(df) # [872094 rows x 51 columns]
 
 
+# Positive Negative Scores
+df[pos_questions] = df[pos_questions].replace({1:-2, 2:-1, 3:0, 4:1, 5:2})
+df[neg_questions] = df[neg_questions].replace({1:2, 2:1, 3:0, 4:-1, 5:-2})
 
+print(df.head()) # [5 rows x 51 columns]
+
+
+# Compute Scores
+traits = {
+    'OPN' : '개방성',
+    'CSN' : '성실성',
+    'EXT' : '외향성', 
+    'AGR' : '친화성',
+    'EST' : '안정성'
+}
+
+for trait, trait_kor in traits.items():
+    trait_cols = sorted([col for col in df.columns if trait in col])
+    df[trait_kor] = df[trait_cols].sum(axis=1)
+
+df_traits = df[list(traits.values()) + ['country']].copy()
+print(df_traits.head())
+'''
+    개방성   성실성   외향성   친화성   안정성 country
+0   15.0       2.0    16.0      9.0     -6.0     GB
+1    5.0       7.0   -10.0     14.0     -5.0     MY
+2   11.0       4.0    -5.0     12.0     -4.0     GB
+3    9.0      -5.0    -4.0      8.0     -1.0     GB
+4   18.0      18.0    -1.0     16.0    -11.0     KE
+'''
+
+# Distribution Plot
+fig = plt.figure(figsize=(16, 6))
+
+for trait in traits.values():
+    sns.distplot(df_traits[trait], kde=False, bins=40, axlabel=False)
+
+fig.legend(traits.values())    
+plt.show()
+
+
+# Openness VS 한국, 미국 개방성 차이 
+fig = plt.figure(figsize=(16, 6))
+
+sns.distplot(df_traits[df_traits['country'] == 'KR']['개방성'], bins=40, axlabel=False)
+sns.distplot(df_traits[df_traits['country'] == 'IT']['개방성'], bins=40, axlabel=False)
+
+fig.legend(['한국', '이탈리아'])    
+plt.show()
+
+
+#Compute Mean by Countries
+df_traits_mean = df_traits.groupby('country').mean().rename_axis('country').reset_index()
+
+print(df_traits_mean) #[113 rows x 6 columns]
+
+
+#COVID-19 Dataset
+df_covid = pd.read_csv('https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv', 
+                        parse_dates=['Date'])
+
+print(df_covid.head())
+print(df_covid.tail())
+
+
+#Country Code
+cc = pd.read_csv('./data/csv/country_code.csv')
+
+print(cc.head())
+
+
+# Filter Dataset Step 1
+df_covid = df_covid[df_covid['Confirmed'] > 50].reset_index(drop=True) 
+
+df_covid = df_covid.groupby(['Country/Region', 'Date']).sum().reset_index()
+
+print(df_covid[df_covid['Country/Region'] == 'US']) # [269 rows x 5 columns]
+
+
+# Filter Dataset Step 2
+n_days = 14
+
+filtered = (
+    datetime.now() - df_covid.groupby('Country/Region')['Date'].min() > timedelta(days=n_days)
+).reset_index().rename(columns = {'Date': 'Filtered'})
+
+filtered_countries = filtered[filtered['Filtered'] == True]['Country/Region']
+
+df_covid = df_covid[df_covid['Country/Region'].isin(filtered_countries)]
+
+df_covid_14days = df_covid.groupby('Country/Region').head(n_days).groupby('Country/Region').tail(1)
+
+print(df_covid_14days) # [180 rows x 5 columns]
+
+
+# Merge All
+df_covid_14days = df_covid_14days.merge(cc, left_on='Country/Region', right_on='Name')
+
+df_covid_14days = df_covid_14days.merge(df_traits_mean, left_on='Code', right_on='country')
+
+print(df_covid_14days.sort_values('Confirmed', ascending=False)) # [96 rows x 13 columns]
+
+
+# Compute Pearson Correlation
+new_df = df_covid_14days[
+    ~df_covid_14days['country'].isin(['CN', 'TR'])
+]
+
+
+for trait, trait_kor in traits.items():
+    fig, axes = plt.subplots(1, 3, figsize=(20, 6))
+
+    for i, n in enumerate(['Confirmed', 'Recovered', 'Deaths']):
+        corr = pearsonr(
+            new_df[trait_kor],
+            new_df[n]
+        )
+
+        sns.regplot(x=trait_kor, y=n, data=new_df, ax=axes[i])
+        axes[i].set_title('%s, %s, :: r=%.2f, p=%.2f' % (trait_kor, n, corr[0], corr[1]))
+
+
+# print(new_df)
+plt.show()
