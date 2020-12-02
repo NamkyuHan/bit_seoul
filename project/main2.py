d8c7814c659bd377b6acd150f74ee7937757c7a2
NamkyuHan <rksh333@naver.com>
2020-12-02 09:42:19 +0900

    2020.12.02 프로젝트 업데이트

--- project/main2.py
+++ project/main2.py
@@ -3,6 +3,7 @@ import plotly.graph_objs as go
 import plotly.offline as py
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly, add_changepoints_to_plot
+import seaborn as sns
 
 import numpy as np
 
@@ -23,18 +24,15 @@ import numpy as np
 # datas = collect_data_files('Cython')
 
 
-# Confirmation, recovery, and death data sets by region worldwide
 # 전세계 지역별 확진자, 회복자, 사망자 Data Set
 url = 'https://raw.githubusercontent.com/datasets/covid-19/master/data/time-series-19-covid-combined.csv'
 data = pd.read_csv(url, error_bad_lines=False)
 
-# Understanding the structure of the data set
+
 # Data Set의 구조 파악
 print(data.head())
 
-
-# Make Korea's confirmed cases timeseries dataframe
-# 한국의 확진자 시계열 데이터프레임를 확인합니다
+# 코로나 확진자 데이터를 한국 기준으로 날짜, 확진자 순으로 만들기
 
 df_korea = data[data['Country/Region'] == 'Korea, South']
 
@@ -44,14 +42,10 @@ df_korea =  pd.DataFrame(df_korea)
 df_korea = df_korea.reset_index().rename(columns={0: 'date', 1: 'confirmed'})
 df_korea = df_korea.drop("index", axis=1)
 
-# df_korea = df_korea.append([
-#   {'date': pd.to_datetime('2020-03-22'), 'confirmed': 8,897}], ignore_index=True)
-
 print(df_korea.tail())
 
 
-# Plot Korean COVID19 confirmed cases.
-# 한국 코로나19 확진자 트렌드를 그래프로 만듭니다.
+# 한국 코로나19 확진자 트렌드를 그래프 제작
 
 fig = go.Figure()
 
@@ -66,8 +60,7 @@ fig.add_trace(
 fig.show()
 
 
-# Make dataframe for Facebook Prophet prediction model.
-# Facebook Prophet 예측 모델에 넣을 데이터프레임을 만들어줍니다.
+# Facebook Prophet 예측 모델에 넣을 데이터프레임 제작
 df_prophet = df_korea.rename(columns={
     'date': 'ds',
     'confirmed': 'y'
@@ -76,31 +69,35 @@ df_prophet = df_korea.rename(columns={
 print(df_prophet.tail())
 
 
-# Make Prophet model including daily seasonality
-# Prophet에서 감안할 계절성을 선택해서 모델을 만듭니다
-m = Prophet(
-    changepoint_prior_scale=0.2, # increasing it will make the trend more flexible
-    changepoint_range=0.9, # place potential changepoints in the first 98% of the time series
-    yearly_seasonality=False,
-    weekly_seasonality=True,
-    daily_seasonality=True,
-    seasonality_mode='additive'
+# Prophet에서 감안할 계절성을 선택해 모델 구성
+model = Prophet(
+    changepoint_prior_scale=0.2, # 트렌드의 유연성 조절
+    changepoint_range=0.9, # 체인지 포인트 설정 가능 범위
+    yearly_seasonality=False, #연 계절성
+    weekly_seasonality=True, # 주 계절성
+    daily_seasonality=True, # 일 계절성
+    seasonality_mode='multiplicative' #multiplicative 데이터의 진폭이 증가하거나 감소함  #additive 데이터의 진폭이 일정함
 )
 
+# prophet으로 핏, 훈련
+model.fit(df_prophet)
 
-m.fit(df_prophet)
+future = model.make_future_dataframe(periods=14)
+# future_data = pd.DataFrame(['2020-11-01', '2020-11-30'], columns=['ds'])
+forecast = model.predict(future)
+forecast[['ds','yhat', 'yhat_lower', 'yhat_upper']].tail()
 
-future = m.make_future_dataframe(periods=14)
-forecast = m.predict(future)
+print(forecast.tail())
 
-fig = plot_plotly(m, forecast)
+fig = plot_plotly(model, forecast)
 py.iplot(fig)
 
 
 # display changepoints as red dotted line on the plot.
 # changepoint를 그래프에 반영해봅시다.
-fig = m.plot(forecast)
-a = add_changepoints_to_plot(fig.gca(), m, forecast)
+fig = model.plot(forecast)
+a = add_changepoints_to_plot(fig.gca(), model, forecast)
+
 
 
 
